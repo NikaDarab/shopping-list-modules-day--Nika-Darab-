cf1f9200f4cdf12c1b040d0312000cb5b8117435
Nika Darab <nika.darab@gmail.com>
2020-11-20 14:20:41 -0600

    findAndDelete

--- scripts/store.js
+++ scripts/store.js
@@ -6,44 +6,51 @@ import item from './item.js';
 let items = [];
 let hideCheckedItems = false;
 
+
 let findById = (id) => {
-  let list = store.items;
-  let found = list.find(item => id = item.id);
+  let list = this.items;
+  let found = list.find(item => id === item.id);
   return found;
 };
 
-let addItem = (name) => {
+let addItem = function(name){
   try {
     item.validateName(name);
-    let newItem=item.create;
-    let list = this.items;
-    list.push(newItem);
-
+    // console.log(newItem);
+    this.items.push(item.create(name));
   } catch (error){
     console.log(`Cannot add item: ${error.message}`);
   }
 };
 
 let findAndToggleChecked = (id) => {
-  let item= this.findById(id);
-  toggleCheckedForListItem(item);
+  let newItem= this.findById(id);
+  newItem.checked = !newItem.checked;
 };
 
 let findAndUpdateName = (id,newName) => {
   try {
     item.validateName(newName);
-    findById(id);
+    let newItem= this.findById(id);
+    newItem.name = name;
   }catch(error){
     console.lof(`Cannot update name: ${error.message}`);
   }
 };
 
+let findAndDelete = function(id) {
+  this.items= this.items.filter(item => item.id !== id);
+};
 
 export default 
 {
   items,
   hideCheckedItems,
+  findById,
+  addItem,
+  findAndToggleChecked,
+  findAndUpdateName,
+  findAndDelete
 };
-
 
 
